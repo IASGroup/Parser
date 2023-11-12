19fcfe5bae96d21303ea19b8097dee60137c0162
Данила Бондаренко <d.b.appeal@gmail.com>
2023-11-12 19:55:58 +0300

    feat(back): Форматтер #inf-25

--- backend/src/Services/Gateway/Program.cs
+++ backend/src/Services/Gateway/Program.cs
@@ -2,38 +2,38 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 new WebHostBuilder()
-	.UseKestrel()
-	.UseContentRoot(Directory.GetCurrentDirectory())
-	.ConfigureAppConfiguration((hostingContext, config) =>
-	{
-		config
-			.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-			.AddJsonFile("appsettings.json", true, true)
-			.AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
-			.AddJsonFile("ocelot.json")
-			.AddEnvironmentVariables();
-	})
-	.ConfigureServices((builder, s) =>
-	{
-		s.AddMvcCore();
-		s.AddEndpointsApiExplorer();
-		s.AddSwaggerForOcelot(builder.Configuration);
-		s.AddOcelot();
-		s.AddSignalR();
-	})
-	// .ConfigureLogging((hostingContext, logging) =>
-	// {
-	// 
-	// })
-	.UseIISIntegration()
-	.Configure(app =>
-	{
-		app.UseSwaggerForOcelotUI(opt =>
-		{
-			opt.PathToSwaggerGenerator = "/swagger/docs";
-		});
-		app.UseWebSockets();
-		app.UseOcelot().Wait();
-	})
-	.Build()
-	.Run();
+    .UseKestrel()
+    .UseContentRoot(Directory.GetCurrentDirectory())
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config
+            .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+            .AddJsonFile("appsettings.json", true, true)
+            .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+            .AddJsonFile("ocelot.json")
+            .AddEnvironmentVariables();
+    })
+    .ConfigureServices((builder, s) =>
+    {
+        s.AddMvcCore();
+        s.AddEndpointsApiExplorer();
+        s.AddSwaggerForOcelot(builder.Configuration);
+        s.AddOcelot();
+        s.AddSignalR();
+    })
+    // .ConfigureLogging((hostingContext, logging) =>
+    // {
+    // 
+    // })
+    .UseIISIntegration()
+    .Configure(app =>
+    {
+        app.UseSwaggerForOcelotUI(opt =>
+        {
+            opt.PathToSwaggerGenerator = "/swagger/docs";
+        });
+        app.UseWebSockets();
+        app.UseOcelot().Wait();
+    })
+    .Build()
+    .Run();
