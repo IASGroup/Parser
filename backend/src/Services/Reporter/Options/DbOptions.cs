406426f4cf0d0124efd9647182da0d50e4c02f70
Данила Бондаренко <d.b.appeal@gmail.com>
2023-11-20 17:41:20 +0300

    feat(back): Добавлено апи получения результатов задачи парсинга #40 (#48)
    * feat(back): Добавлено апи получения результатов задачи парсинга #40
    
    * feat(back): Форматирование #40

---
+++ backend/src/Services/Reporter/Options/DbOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManager.Options;
+
+public class DbOptions
+{
+	public static string Name = nameof(DbOptions);
+	public string Host { get; set; }
+	public string DbName { get; set; }
+	public string Port { get; set; }
+	public string UserName { get; set; }
+	public string UserPassword { get; set; }
+}
