53834ab033375a5220a2f3cdfff9256221858335
Danisimo <savenkov-lokin2013@yandex.ru>
2023-11-26 17:10:24 +0300

    Разработан route для получения части результата, выполненной задачи

---
+++ backend/src/Services/Reporter/ParserTask/Queries/GetPartTaskResultQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Reporter.ParserTask.Contracts;
+
+namespace Reporter.ParserTask.Queries;
+
+public class GetPartTaskResultQuery : IRequest<Result<byte[]>>
+{
+	public Guid TaskId { get; set; }
+	public Guid ResultId { get; set; }
+}
