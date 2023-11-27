0fad8aa005ed80a574994f95df2f3de0c997ee10
Данила Бондаренко <ser4iosmirnovi4@gmail.com>
2023-11-27 20:24:47 +0300

    feat(back): Фикс по ревью #12

--- backend/src/Services/Unlocker/Controllers/UnlockerController.cs
+++ backend/src/Services/Unlocker/Controllers/UnlockerController.cs
@@ -1,80 +1,95 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Text.RegularExpressions;
+using Unlocker.Contracts;
 
 namespace Unlocker.Controllers
 {
-    [ApiController]
-    [Route("api/tor")]
-    public class UnlockerController : Controller
-    {
-        private readonly HttpClient _httpClient;
-        private readonly TorControl _torControl;
+	[ApiController]
+	[Route("api/tor")]
+	public class UnlockerController : Controller
+	{
+		private readonly HttpClient _httpClient;
+		private readonly TorControl _torControl;
 
-        public UnlockerController()
-        {
-            var handler = new HttpClientHandler
-            {
-                Proxy = new WebProxy("socks5://localhost:9050")
-            };
+		public UnlockerController()
+		{
+			var handler = new HttpClientHandler
+			{
+				Proxy = new WebProxy("socks5://localhost:9050")
+			};
 
-            _httpClient = new HttpClient(handler);
-            _torControl = new TorControl("localhost", 9050, "");
-        }
+			_httpClient = new HttpClient(handler);
+			_httpClient.Timeout = TimeSpan.FromSeconds(10);
+			_torControl = new TorControl("localhost", 9050, "");
+		}
 
-        [HttpGet("download")]
-        public async Task<IActionResult> GetTorContentAsync([FromQuery] string url)
-        {
-            try
-            {
-                var response = await _httpClient.GetAsync(url);
-                response.EnsureSuccessStatusCode();
-                string content = await response.Content.ReadAsStringAsync();
-                return Content(content);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest($"Failed to get URL content by Tor: {ex.Message}");
-            }
-        }
+		[HttpPost("download")]
+		public async Task<IActionResult> GetTorContentAsync([FromBody] HttpRequestDto requestDto)
+		{
+			try
+			{
+				var request = new HttpRequestMessage()
+				{
+					RequestUri = new Uri(requestDto.BaseUrl),
+					Method = requestDto.MethodName.ToUpper() switch
+					{
+						"GET" => HttpMethod.Get,
+						"POST" => HttpMethod.Post,
+						"PUT" => HttpMethod.Put,
+						"DELETE" => HttpMethod.Delete,
+						"PATCH" => HttpMethod.Patch,
+						_ => throw new ArgumentException("Метод запроса не найден", nameof(requestDto.MethodName))
+					},
+					Content = requestDto.Body is null ? null : new StringContent(requestDto.Body)
+				};
+				foreach (var header in requestDto.Headers)
+				{
+					request.Headers.Add(header.Name, header.Value);
+				}
 
-        [HttpPost("changeip")]
-        public async Task<IActionResult> ChangeTorIPAsync()
-        {
-            try
-            {
-                await _torControl.SignalNewNymAsync();
+				var response = await _httpClient.SendAsync(request);
+				response.EnsureSuccessStatusCode();
+				var content = await response.Content.ReadAsStringAsync();
+				return Content(content);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest($"Failed to get URL content by Tor: {ex.Message}");
+			}
+		}
 
-                await Task.Delay(5000);
+		[HttpPost("changeip")]
+		public async Task<IActionResult> ChangeTorIPAsync()
+		{
+			try
+			{
+				await _torControl.SignalNewNymAsync();
 
-                return Ok(GetTorIpAddress());
-            }
-            catch (Exception ex)
-            {
-                return BadRequest($"Failed to change Tor IP: {ex.Message}");
-            }
-        }
+				await Task.Delay(5000);
 
-        [HttpGet("gettorip")]
-        public string GetTorIpAddress()
-        {
-            try
-            {
-                var response = _httpClient.GetAsync("https://check.torproject.org/").Result;
-                var result = response.Content.ReadAsStringAsync().Result;
+				return Ok(GetTorIpAddress());
+			}
+			catch (Exception ex)
+			{
+				return BadRequest($"Failed to change Tor IP: {ex.Message}");
+			}
+		}
 
-                var regex = new Regex(@"([0-9]?[0-9]?[0-9]\.[0-9]?[0-9]?[0-9]\.[0-9]?[0-9]?[0-9]\.[0-9]?[0-9]?[0-9])");
-                var match = regex.Match(result);
-                string extractedIP;
-
-                extractedIP = match.Value;
-
-                return extractedIP;
-            }
-            catch (Exception ex)
-            {
-                return $"Failed to get Tor IP address: {ex.Message}";
-            }
-        }
-    }
+		private string GetTorIpAddress()
+		{
+			try
+			{
+				var response = _httpClient.GetAsync("https://check.torproject.org/").Result;
+				var result = response.Content.ReadAsStringAsync().Result;
+				var regex = new Regex(@"([0-9]?[0-9]?[0-9]\.[0-9]?[0-9]?[0-9]\.[0-9]?[0-9]?[0-9]\.[0-9]?[0-9]?[0-9])");
+				return regex.Match(result).Value;
+			}
+			catch (Exception ex)
+			{
+				return $"Failed to get Tor IP address: {ex.Message}";
+			}
+		}
+	}
 }
--- backend/src/Services/Unlocker/TorControl.cs
+++ backend/src/Services/Unlocker/TorControl.cs
@@ -3,34 +3,34 @@ using System.Text;
 
 namespace Unlocker
 {
-    public class TorControl
-    {
-        private readonly string _address;
-        private readonly int _port;
-        private readonly string _password;
+	public class TorControl
+	{
+		private readonly string _address;
+		private readonly int _port;
+		private readonly string _password;
 
-        public TorControl(string address, int port, string password)
-        {
-            _address = address;
-            _port = port;
-            _password = password;
-        }
+		public TorControl(string address, int port, string password)
+		{
+			_address = address;
+			_port = port;
+			_password = password;
+		}
 
-        public async Task SendCommandAsync(string command)
-        {
-            using var client = new TcpClient(_address, _port);
-            using var stream = client.GetStream();
-            string authCommand = $"AUTHENTICATE \"{_password}\"\r\n";
-            byte[] authBuffer = Encoding.ASCII.GetBytes(authCommand);
-            await stream.WriteAsync(authBuffer);
+		private async Task SendCommandAsync(string command)
+		{
+			using var client = new TcpClient(_address, _port);
+			await using var stream = client.GetStream();
+			var authCommand = $"AUTHENTICATE \"{_password}\"\r\n";
+			var authBuffer = Encoding.ASCII.GetBytes(authCommand);
+			await stream.WriteAsync(authBuffer);
 
-            byte[] commandBuffer = Encoding.ASCII.GetBytes($"{command}\r\n");
-            await stream.WriteAsync(commandBuffer);
-        }
+			var commandBuffer = Encoding.ASCII.GetBytes($"{command}\r\n");
+			await stream.WriteAsync(commandBuffer);
+		}
 
-        public async Task SignalNewNymAsync()
-        {
-            await SendCommandAsync("SIGNAL NEWNYM");
-        }
-    }
+		public async Task SignalNewNymAsync()
+		{
+			await SendCommandAsync("SIGNAL NEWNYM");
+		}
+	}
 }
